6f27b99a4bfa8f5847bd4745313419d9b9ab6f7b
joan Njeri <joanwanjiru.1234@gmail.com>
2022-06-16 22:13:53 +0300

    added hackerrank solution

--- .SimpleArrSum.py
+++ .SimpleArrSum.py
@@ -1,10 +1,46 @@
-ar_count = int(input().strip())
-def simpleArraySum():
-    ar = []
-    for i in range(1,ar_count+1):
-        ar.append(i)
-    total = 0
+"""My thinking through the problem"""
+# ar_count = int(input().strip())
+# def simpleArraySum():
+#     ar = []
+#     for i in range(1,ar_count+1):
+#         ar.append(i)
+#     total = 0
+#     for i in ar:
+#         total += i
+#     return total
+# print(simpleArraySum())     
+
+"""Hackerrank solution"""
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    additems = 0
     for i in ar:
-        total += i
-    return total
-print(simpleArraySum())
+        additems +=i
+    return additems
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
